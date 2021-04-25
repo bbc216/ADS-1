970ef1737c4593b3310a85c751c81a5dac0eb0d0
bbc216 <78923983+bbc216@users.noreply.github.com>
2021-04-25 18:05:27 +0300

    20-ист-2 Бабасян

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,20 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int a = 0, b = size - 1, k = 0;
+while (a < b) {
+int mid = (a + b) / 2;
+if (arr[mid] < value) a = mid + 1;
+else b = mid;
+}
+if (arr[a] == value) {
+while (arr[a] == value) {
+k++;
+a++;
+}
+}
+if (k != 0)
+return k;
+else
+return 0;
 }
